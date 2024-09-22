cd973d4f2fc402d3c6be855a555b98dc26157f83
strayasi <015112395909@Fr>
2024-09-22 21:38:06 +0200

    deploying10

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ramigpt/', // Update this line
+  //base: '/ramigpt/', // Update this line
 });
