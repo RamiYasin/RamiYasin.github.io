f2b80a57272cd0a3010b151f1e30812e2ecc9417
strayasi <015112395909@Fr>
2024-09-22 21:32:16 +0200

    deploying8

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import WelcomePage from './WelcomePage'; // Adjust the import as necessary
+import WelcomePage from './welcomePage'; // Adjust the import as necessary
 import ChatPage from './ChatScreen'; // Replace with your actual chat page component
 
 function App() {
