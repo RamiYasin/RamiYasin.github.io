5a9aa56ea23c6e3f5f6e574898d0dd2e2e1d44d3
strayasi <015112395909@Fr>
2024-09-22 20:11:21 +0200

    deploying2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/RamiYasin.github.io/'
-})
+  base: '/ramigpt/',  // This should match your project’s deployment path
+});
